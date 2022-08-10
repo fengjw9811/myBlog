5ec2e88b62c4b0d09a306df75910ff2c345fbdef
fengjw_mac <fengjw666@gmail.com>
2022-08-11 03:37:56 +0800

    feat: 实现cookie和session登录

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const { access } = require('./src/utils/log')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+const SESSION_DATA = {}
+
+// 获取cookie的过期时间
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 100))
+  console.log(d.toGMTString())
+  return d.toGMTString()
+}
+
 const getPostData = (req) => {
   const promise = new Promise((resolve, reject) => {
     if (req.method !== 'POST') {
@@ -44,17 +54,30 @@ const serverHandle = (req, res) => {
 
   // 解析cookie
   req.cookie = {}
-  const cookieStr = req.headers.cookie || ''
+  const cookieStr = req.headers.cookie || '' // k1=v1;k2=v2;k3=v3
   cookieStr.split(';').forEach(item => {
     if (!item) {
       return
     }
     const arr = item.split('=')
-    const key = arr[0]
-    const val = arr[1]
+    const key = arr[0].trim()
+    const val = arr[1].trim()
     req.cookie[key] = val
   })
-  console.log('cookie', req.cookie)
+
+  // 解析 session
+  let needSetCookie = false
+  let userId = req.cookie.userid
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {}
+    }
+  } else {
+    needSetCookie = true
+    userId = `${Date.now()}_${Math.random()}`
+    SESSION_DATA[userId] = {}
+  }
+  req.session = SESSION_DATA[userId]
 
   // 处理postData
   getPostData(req).then(postData => {
@@ -63,6 +86,9 @@ const serverHandle = (req, res) => {
     const blogResult = handleBlogRouter(req, res)
     if (blogResult) {
       blogResult.then(blogData => {
+        if (needSetCookie) {
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
         res.end(JSON.stringify(blogData))
       })
       return
@@ -72,6 +98,9 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res)
     if (userResult) {
       userResult.then(userData => {
+        if (needSetCookie) {
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
         res.end(JSON.stringify(userData))
       })
       return
--- src/router/user.js
+++ src/router/user.js
@@ -5,11 +5,16 @@ const handleUserRouter = (req, res) => {
   const method = req.method
 
   // 登录
-  if (method === 'POST' && req.path === '/api/user/login') {
-    const { username, password } = req.body
+  if (method === 'GET' && req.path === '/api/user/login') {
+    // const { username, password } = req.body
+    const { username, password } = req.query
     const result = login(username, password)
     return result.then(data => {
       if (data.username) {
+        // 设置session
+        req.session.username = data.username
+        req.session.realname = data.realname
+        console.log('req.session is ', req.session)
         return new SuccessModel()
       }
       return new ErrorModel('登录失败')
@@ -18,8 +23,11 @@ const handleUserRouter = (req, res) => {
 
   // 登录验证的测试
   if (method === 'GET' && req.path === '/api/user/login-test') {
-    if (req.cookie.username) {
-      return Promise.resolve(new SuccessModel())
+    console.log(req.session)
+    if (req.session.username) {
+      return Promise.resolve(new SuccessModel({
+        session: req.session
+      }))
     }
     return Promise.resolve(new ErrorModel('尚未登录'))
   }
