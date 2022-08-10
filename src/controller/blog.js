797c1b334fad0c98a1882cd30e5cb156c85bf43f
fengjw <2849425293@qq.com>
2022-08-10 16:38:43 +0800

    feat: xss攻击预防

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,3 +1,4 @@
+const xss = require('xss')
 const { exec } = require('../db/mysql')
 
 const getList = (author, keyword) => {
@@ -21,7 +22,9 @@ const getDetail = (id) => {
 }
 
 const newBlog = (blogData = {}) => {
-  const { title, content, author } = blogData
+  const title = xss(blogData.title)
+  const content = xss(blogData.content)
+  const author = xss(blogData.author)
   const createTime = Date.now()
 
   const sql = `insert into blogs (title, content, createtime, author) values ('${title}', '${content}', ${createTime}, '${author}') `
@@ -34,7 +37,8 @@ const newBlog = (blogData = {}) => {
 }
 
 const updateBlog = (id, blogData = {}) => {
-  const { title, content } = blogData
+  const title = xss(blogData.title)
+  const content = xss(blogData.content)
   const sql = `update blogs set title='${title}', content='${content}' where id=${id} `
   return exec(sql).then(updateData => {
     // console.log('updateData', updateData)
